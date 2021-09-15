df419f01cc106e39bb0bbc85de27653bcc10e770
Ben Chatelain <ben@octop.ad>
2021-09-14 21:48:07 -0600

    🔧 Configure package

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,14 +1,15 @@
-"use strict"
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+export default async function (context, req) {
+    "use strict"
+    context.log('JavaScript HTTP trigger function processed a request.')
+
+    const name = (req.query.name || (req.body && req.body.name))
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
-    };
+    }
 }
